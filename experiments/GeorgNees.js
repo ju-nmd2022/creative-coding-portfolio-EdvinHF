eb3e6eff5594ccbc90bab088c90d2643c78de4f0
EdvinHF <Hoed22rl@student.ju.se>
2024-08-29 21:50:12 +0200

    rotatate

--- experiments/GeorgNees.js
+++ experiments/GeorgNees.js
@@ -5,22 +5,25 @@ function setup() {
 let cols = 10;
 let rows = 15;
 let squareSize = 40;
+const c = color(255, 99, 71);
 
 function draw() {
+  strokeWeight(1);
   noFill();
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       let angle = random(-PI / 4, PI / 4) * y * 0.1 + random(-0.01, 0.01);
       push();
-      translate(x * squareSize + 110, y * squareSize + 110);
-      const c = color(255, 127, 80);
-      const alpha = 1;
+      translate(x * squareSize + 90, y * squareSize + 110);
+
+      let alpha = (9 / (y + x + 0.001)) * 40;
       c.setAlpha(alpha);
       stroke(c);
       rotate(angle);
       rectMode(CENTER);
       rect(x, y, squareSize, squareSize);
       pop();
+      rotate(-0.001);
     }
   }
   noLoop();
